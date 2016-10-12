1ba84781d5870b7043af6b1d66fe2ee5bf42821d
ceul <carlos.urrego@ucp.edu.co>
2016-10-12 12:20:57 -0500

    Se realizaron modificaciones
    en el cliente se crearon dos hilos uno para escuchar y otro para enviar lo que permite una comunicacion full duplex de parte del cliente.
    
    en el servidor se soluciono el problema a la hora que se cerraba una conexion, esto se hizo a traves de la bandera 'flag' y ademas esta misma permite, terminar el hilo para asi no consumir recursos.

--- chat-client.py
+++ chat-client.py
@@ -1,15 +1,28 @@
 from socket import *
+import threading
 
 def client():
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     addr = ('', 3457)
     clientSocket.connect(addr)
+    sending=threading.Thread(target=client_send, args=(clientSocket, ))
+    listening=threading.Thread(target=client_listening, args=(clientSocket, ))
+    listening.start()
+    sending.start()
+
+
+def client_send(clientSocket, ):
+    print "entro"
     while True:
         message = raw_input("ingrese el exto a enviar: ")
         clientSocket.send(message+'\n')
-        #data, server = clientSocket.recvfrom(1024)
-        #print (data)
             #clientSocket.close()
-    print "ejecuto"
+
+def client_listening(clientSocket, ):
+    print"listening"
+    while True:
+        data, server = clientSocket.recvfrom(1024)
+        print (data)
+
 client()
--- chat-server.py
+++ chat-server.py
@@ -15,11 +15,14 @@ def server():
         thread.start_new_thread(new_client, (client_connection, client_address,client_list))
 
 def new_client(client_connection, client_address,client_list):
-    while True:
-        message = client_connection.recv(1024)#recive la conexion
+    flag=False
+    while flag==False:
+        message = client_connection.recv(1024)#recibe la conexion
         if message =='':
-            client_list.close()
+            client_list.remove(client_connection)
+            client_connection.close()
             print "Se cerro la conexion"
+            flag=True
         else:
             broadcast(client_list,message,client_connection)
             print message
@@ -31,8 +34,6 @@ def broadcast(client_list,message, actual_client):
         if client == actual_client:
             continue
         else:
-        #client_connection=client_list[client]
             client.sendall(message)# Envia la respuesta al cliente
-    #client_connection.close()#Se cierra la conexion
 
 server()
