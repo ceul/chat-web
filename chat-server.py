1f01ab1eaf7763784a39a94460f77a0169e65b83
ceul <carlos.urrego@ucp.edu.co>
2016-11-17 15:14:08 -0500

    Add files via upload

--- chat-server.py
+++ chat-server.py
@@ -1,5 +1,10 @@
 from socket import *
 import thread
+import MySQLdb
+from smtplib import SMTP
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from random import SystemRandom
 
 def server():
     client_list=[]
@@ -23,6 +28,22 @@ def new_client(client_connection, client_address,client_list):
             client_connection.close()
             print "Se cerro la conexion"
             flag=True
+        elif (message.startswith('Login:'))==True:
+            print message
+            pos1=(message.find('User:',7)+5)
+            pos2=(message.find('\n',pos1))
+            user= message[pos1:pos2]
+            pos3=message.find('Passw:',pos2)+6
+            pos4=(message.find('\n',pos3))
+            passw=message[pos3:pos4]
+            res=compdatabase(user,passw)
+            client_connection.sendall(res)
+        elif (message.startswith('Register:'))==True:
+            print message
+            pos1=(message.find('User:',10)+5)
+            pos2=(message.find('\n',pos1))
+            user= message[pos1:pos2]
+            IngresarUsuario(user)
         else:
             broadcast(client_list,message,client_connection)
             print message
@@ -35,5 +56,67 @@ def broadcast(client_list,message, actual_client):
             continue
         else:
             client.sendall(message)# Envia la respuesta al cliente
+
+def compdatabase(user,password):
+    bd = MySQLdb.connect("localhost", "root", "admin", "bd_chat")
+    cursor = bd.cursor()
+    sql = 'SELECT correo,contrasena FROM usuario WHERE correo="%s" AND contrasena="%s"' % (user, password)
+    rows_count = cursor.execute(sql)
+    resultados = cursor.fetchall()
+    print resultados
+    if rows_count > 0:
+        resultado = cursor.fetchall()
+        print "El usuario esta registrado"
+        return 'ok'
+    else:
+        print "El usuario no esta registrado"
+        return 'no'
+    bd.close()
+
+def IngresarUsuario(user):
+    def database(user, p):
+        bd = MySQLdb.connect("localhost", "root", "admin", "bd_chat")
+        # Preparamos el cursor que nos va a ayudar a realizar las operaciones con la base de datos
+        cursor = bd.cursor()
+        # Preparamos el query SQL para insertar un registro en la BD
+        sql = 'INSERT INTO usuario (correo,contrasena) VALUES("%s","%s")' % (user, p)
+        try:
+            cursor.execute(sql)
+            bd.commit()
+            print("\n Se inserto con exito")
+        except:
+            print("Hubo un problema")
+            bd.rollback()
+        bd.close()
+    pass
+    ##########  INICIO DATOS CORREO  #############
+    # establecer conexion con el servidor de gmail
+    mailserver = SMTP('smtp.gmail.com', 587)
+    mailserver.ehlo()
+    mailserver.starttls()
+    mailserver.ehlo()
+    mailserver.login("omgchat1.0@gmail.com", "ucatolica123")
+    # construccion del mensaje
+    mensaje = MIMEMultipart()
+    mensaje['from'] = "omgchat1.0@gmail.com"
+    mensaje['to'] = user
+    mensaje['subject'] = "Confirmacion de cuenta OMGChat"
+    longitud = 10
+    valores = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    cryptogen = SystemRandom()
+    p = ""
+    while longitud > 0:
+        p = p + cryptogen.choice(valores)
+        longitud = longitud - 1
+    mensaje.attach(MIMEText("Saludos usuario, envio confirmacion de correo:\n Su contrasena es:" + p))
+    database(user,p)
+    # Enviar correo
+    try:
+        mailserver.sendmail(mensaje['from'], mensaje['to'], mensaje.as_string())
+        print ("Envio correo")
+    except:
+        print("Error en envio del correo")
+    # Cerrar conexion
+    mailserver.close()
 
 server()
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import MySQLdb
 from socket import *
 from kivy.uix.progressbar import ProgressBar
 import threading
-import os
+
 
 Builder.load_string("""
 <ChatLabel@Label>:
@@ -79,58 +79,13 @@ Builder.load_string("""
                     height: '45dp'
                     pos_hint: {'center_x': .5, 'center_y': .28}
                     text: 'Iniciar Sesion'
-                    on_press: root.compdatabase(user.text,passw.text)#aqui se envia la informacion que hay en los text inputs para ser procesadas
+                    on_press: root.connect(user.text,passw.text)
                 Button:
                     size_hint_y: None
                     height: '45dp'
                     pos_hint: {'center_x': .5, 'center_y': .15}
                     text: 'Registrarse'
                     on_press: root.manager.current = 'register'
-<LoginChatScreen>:
-    canvas.before:
-        Color:
-            rgb: (0.5, 0.5, 0.5, 1.0)
-        Rectangle:
-            pos: self.pos
-            size: self.size
-    FloatLayout:
-        Image:
-            source: '/home/ceul/app/chat-web-master/icono.png'
-            size: self.texture_size
-            height: '55dp'
-            size_hint_y: None
-            pos_hint: {'center_x': .5, 'center_y': .8}
-    FloatLayout:
-        orientation: 'vertical'
-        FloatLayout:
-            orientation: 'vertical'
-            Label:
-                text:'Usuario'
-                pos_hint: {'center_x': .5, 'center_y': .65}
-            TextInput:
-                id:user
-                size_hint_y: None
-                height: '32dp'
-                text: 'Ingrese su correo'
-                focus: True
-                pos_hint: {'center_x': .5, 'center_y': .55}
-            Label:
-                text:'Contrasena'
-                pos_hint: {'center_x': .5, 'center_y': .47}
-            TextInput:
-                id:passw
-                size_hint_y: None
-                height: '32dp'
-                focus: True
-                password:True
-                pos_hint: {'center_x': .5, 'center_y': .4}
-            FloatLayout:
-                Button:
-                    size_hint_y: None
-                    height: '45dp'
-                    pos_hint: {'center_x': .5, 'center_y': .26}
-                    text: 'Iniciar Sesion'
-                    on_press: root.compdatabase1(user.text,passw.text)
 
 <ChatScreen>:
     on_enter: root.recvMsg(chat_logs)
@@ -229,53 +184,10 @@ Builder.load_string("""
 # Declare both screens
 
 class LoginScreen(Screen):
-
-    def compdatabase(self,user,password):
-
-        bd = MySQLdb.connect("localhost", "root", "admin", "bd_chat")
-
-        # Preparamos el cursor que nos va a ayudar a realizar las operaciones con la base de datos
-        cursor = bd.cursor()
-
-        # Preparamos el query SQL para insertar un registro en la BD
-        sql = 'SELECT correo,contrasena FROM usuario WHERE correo="%s" AND contrasena="%s"' % (user, password)
-
-        rows_count = cursor.execute(sql)
-        resultados = cursor.fetchall()
-        print resultados
-        if rows_count > 0:
-            resultado = cursor.fetchall()
-            print "El usuario esta registrado"
-            conn.connect()
-            self.manager.current = 'chat'
-        else:
-            print "El usuario no esta registrado"
-        bd.close()
-    pass
-
-class LoginChatScreen(Screen):
-
-    def compdatabase1(self,user,password):
-
-        bd = MySQLdb.connect("localhost", "root", "admin", "bd_chat")
-        # Preparamos el cursor que nos va a ayudar a realizar las operaciones con la base de datos
-        cursor = bd.cursor()
-
-        # Preparamos el query SQL para insertar un registro en la BD
-        sql = 'SELECT correo,contrasena FROM usuario WHERE correo="%s" AND contrasena="%s"' % (user, password)
-
-        rows_count = cursor.execute(sql)
-        resultados = cursor.fetchall()
-        print resultados
-        if rows_count > 0:
-            resultado = cursor.fetchall()
-            print "El usuario esta registrado"
-            conn.connect()
-            self.manager.current = 'chat'
-        else:
-            print "El usuario no esta registrado"
-        bd.close()
-    pass
+    def connect(self,user,password):
+        ans=conn.logindb(user,password)
+        if ans=='1':
+            self.manager.current='chat'
 
 class ChatScreen(Screen):
     def recvMsg(self,chat_logs_o):
@@ -293,56 +205,11 @@ class FileScreen(Screen):
 
 class RegisterScreen(Screen):
     def IngresarUsuario(self,user):
-        def database(user, p):
-            bd = MySQLdb.connect("localhost", "root", "admin", "bd_chat")
-            # Preparamos el cursor que nos va a ayudar a realizar las operaciones con la base de datos
-            cursor = bd.cursor()
-            # Preparamos el query SQL para insertar un registro en la BD
-            sql = 'INSERT INTO usuario (correo,contrasena) VALUES("%s","%s")' % (user, p)
-            try:
-                cursor.execute(sql)
-                bd.commit()
-                print("\n Se inserto con exito")
-            except:
-                print("Hubo un problema")
-                bd.rollback()
-            bd.close()
-        pass
-        ##########  INICIO DATOS CORREO  #############
-        # establecer conexion con el servidor de gmail
-        mailserver = SMTP('smtp.gmail.com', 587)
-        mailserver.ehlo()
-        mailserver.starttls()
-        mailserver.ehlo()
-        mailserver.login("omgchat1.0@gmail.com", "ucatolica123")
-        # construccion del mensaje
-        mensaje = MIMEMultipart()
-        mensaje['from'] = "omgchat1.0@gmail.com"
-        mensaje['to'] = user
-        mensaje['subject'] = "Confirmacion de cuenta OMGChat"
-        longitud = 10
-        valores = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        cryptogen = SystemRandom()
-        p = ""
-        while longitud > 0:
-            p = p + cryptogen.choice(valores)
-            longitud = longitud - 1
-        mensaje.attach(MIMEText("Saludos usuario, envio confirmacion de correo:\n Su contrasena es:" + p))
-        database(user,p)
-        # Enviar correo
-        try:
-            mailserver.sendmail(mensaje['from'], mensaje['to'], mensaje.as_string())
-            print ("Envio correo")
-        except:
-            print("Error en envio del correo")
-        # Cerrar conexion
-        mailserver.close()
-    pass
+        conn.registerdb(user)
 
 # Create the screen manager
 sm = ScreenManager()
 sm.add_widget(LoginScreen(name='login'))
-sm.add_widget(LoginChatScreen(name='login2'))
 sm.add_widget(ChatScreen(name='chat'))
 sm.add_widget(RegisterScreen(name='register'))
 sm.add_widget(FileScreen(name='file'))
@@ -363,6 +230,15 @@ class OMGChatApp(App):
             self.clientSocket.send('%s' % ( msg))
             chat_logs.text += ('yo: %s\n' % ( msg))
             message.text = ''
+
+    def registerdb(self,user):
+        self.clientSocket.send('Register:\nUser:%s\n' % ( user))
+    def logindb(self,user,password):
+        self.clientSocket.send('Login:\nUser:%s\nPassw:%s\n' % ( user,password))
+        data, server = self.clientSocket.recvfrom(1024)
+        if data=='ok':
+            print 'entro'
+            return '1'
 
     def send_file(self,path):
         with open(path, "rb") as archivo:
@@ -392,5 +268,6 @@ class OMGChatApp(App):
                 chat_logs_o.text += ('otro: %s\n' % ( data))
 
 conn=OMGChatApp()
+conn.connect()
 if __name__ == '__main__':
         OMGChatApp().run()
